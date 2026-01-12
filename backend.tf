47ed4dfed2cb1c2bd3284e26ab04a0b1081745e4
equiroz1995 <elizabethgaete1995@gmail.com>
2026-01-12 15:04:20 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,8 @@
 terraform {
   backend "azurerm" {
-      
+    resource_group_name  = "rg-poc-test-001"
+    storage_account_name = "stapocchl001"
+    container_name       = "tfstate"
+    # key = "app-services/chile.tfstate"
   }
 }
